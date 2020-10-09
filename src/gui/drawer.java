f75103ff07b0959759473ca0fdc941dcf20da767
GONG Zifan <zifangong2-c@my.cityu.edu.hk>
2020-10-09 13:01:23 +0800

    1
    try commit

--- src/gui/drawer.java
+++ src/gui/drawer.java
@@ -15,18 +15,12 @@ public class drawer {
 		g.setColor(type.getBaseColor());
 		g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
 		
-		/*
-		 * Fill the bottom and right edges of the tile with the dark shading color.
-		 */
+		
 		g.setColor(type.getDarkColor());
 		g.fillRect(x, y + TILE_SIZE - SHADE_WIDTH, TILE_SIZE, SHADE_WIDTH);
 		g.fillRect(x + TILE_SIZE - SHADE_WIDTH, y, SHADE_WIDTH, TILE_SIZE);
 		
-		/*
-		 * Fill the top and left edges with the light shading. We draw a single line
-		 * for each row or column rather than a rectangle so that we can draw a nice
-		 * looking diagonal where the light and dark shading meet.
-		 */
+		
 		g.setColor(type.getLightColor());
 		for(int i = 0; i < SHADE_WIDTH; i++) {
 			g.drawLine(x, y + i, x + TILE_SIZE - i - 1, y + i);
